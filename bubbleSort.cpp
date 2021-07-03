fe251827125f0af178ab64aa7a2b7376e1d5d490
henryRiveraCS <44453113+henryriveraCS@users.noreply.github.com>
2021-07-03 15:27:42 -0400

    added bubble sort

---
+++ bubbleSort.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int arrSize)
+{
+	for(int i=0; i<arrSize; i++){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+//used to swap 2 values
+void swap(int* a, int* b)
+{
+	cout << "SWAPPING: " << *a << " AND " << *b << endl;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void bubbleSort(int arr[], int arrSize)
+{
+	int i;
+	int j;
+
+	//iterate through the entire array
+	for(i=0; i < arrSize; i++)
+	{
+		//iterate through array again (but keep track of lowest index with lowest_idx and swap if necessary)
+		for(j=0; j < arrSize; j++)
+		{
+			//cout << "CURRENT AT: " << arr[j] << endl;
+			//cout << "LOOKING AT: " << arr[j+1] << endl;
+			// if current index greater than next index -> swap
+			if(arr[j] > arr[j+1])
+			{
+				swap(&arr[j], &arr[j+1]);
+				printArray(arr, arrSize);
+				cout << endl;
+			}
+		}
+	}
+}
+
+int main()
+{
+	cout << "BUBBLE SORT" << endl;
+
+	//array info
+	int arrSize = 6;
+	int arr[arrSize] = {5, 1, 4, 2, 8, 3};
+	
+	//unorganized array:
+	cout << "UNORGANIZED ARRAY: ";
+	printArray(arr, arrSize);
+	
+	//run sort algorithm
+	bubbleSort(arr, arrSize);
+
+	//organized array
+	cout << "FINAL ARRAY: ";
+	printArray(arr, arrSize);
+
+	return 0;
+}
